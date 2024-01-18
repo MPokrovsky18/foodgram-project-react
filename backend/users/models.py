0075a254f1d724e8c739677d174037be8570e6ea
Maxim Pokrovsky <maxim.pokrovsky1807@gmail.com>
2024-01-18 17:07:26 +0300

    Add fields to User.

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,5 +1,33 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+MAX_CHARFIELD_LENGTH = 150
+MAX_EMAIL_LENGTH = 254
 
 
 class FoodgramUser(AbstractUser):
-    pass
+    email = models.EmailField(
+        'Почта', max_length=MAX_EMAIL_LENGTH, unique=True
+    )
+    username = models.CharField(
+        'Имя пользователя', max_length=MAX_CHARFIELD_LENGTH, unique=True
+    )
+    first_name = models.CharField('Имя', max_length=MAX_CHARFIELD_LENGTH)
+    last_name = models.CharField('Фамилия', max_length=MAX_CHARFIELD_LENGTH)
+    subscriptions = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        related_name='subscribers',
+        verbose_name='Подписки'
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    class Meta:
+        ordering = ('email',)
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'ПОльзователи'
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
