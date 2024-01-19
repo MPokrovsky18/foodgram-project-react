424455912aa69f08264956c0830c079c3d75912f
Maxim Pokrovsky <maxim.pokrovsky1807@gmail.com>
2024-01-19 14:24:38 +0300

    Create migrates with new models.

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,9 +1,9 @@
-from django.contrib.auth import get_user_model
+from django.conf import settings
 from django.db import models
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
 
-User = get_user_model()
+User = settings.AUTH_USER_MODEL
 
 
 class Tag(models.Model):
