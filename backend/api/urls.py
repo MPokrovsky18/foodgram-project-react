52866094dfb66a50ef04a7397f9af9c7a367b542
Maxim Pokrovsky <maxim.pokrovsky1807@gmail.com>
2024-01-25 13:22:22 +0300

    Enabled interaction with the API

---
+++ backend/api/urls.py
@@ -0,0 +1,36 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from api.views import (
+    IngredientReadOnlyViewSet, TagReadOnlyViewSet, RecipeViewSet
+)
+
+
+namespace = 'api'
+
+router_v1 = DefaultRouter()
+router_v1.register(
+    r'tags',
+    TagReadOnlyViewSet,
+    basename='tags'
+)
+router_v1.register(
+    r'ingredients',
+    IngredientReadOnlyViewSet,
+    basename='ingredients'
+)
+router_v1.register(
+    r'recipes',
+    RecipeViewSet,
+    basename='recipes'
+)
+
+urlpatterns_v1 = [
+    path('', include('djoser.urls')),
+    path('', include('djoser.urls.jwt')),
+    path('', include(router_v1.urls)),
+]
+
+urlpatterns = [
+    path('', include(urlpatterns_v1)),
+]
