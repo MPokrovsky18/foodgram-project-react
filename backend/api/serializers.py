c0ef09859275caedc595416209af2ceebbc9b06f
Maxim Pokrovsky <maxim.pokrovsky1807@gmail.com>
2024-01-26 18:29:15 +0300

    Update interactive with user from recipe serializer.

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,30 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from recipes.models import Ingredient, Recipe, IngredientInRecipe, Tag
+
+
+class UserSerializer(serializers.ModelSerializer):
+    is_subscribed = serializers.SerializerMethodField()
+
+    class Meta:
+        model = get_user_model()
+        fields = (
+            'email',
+            'id',
+            'username',
+            'first_name',
+            'last_name',
+            'is_subscribed',
+        )
+
+    def get_is_subscribed(self, user):
+        current_user = self.context.get('request').user
+
+        if current_user and current_user.is_authenticated:
+            return user.is_subscriber(current_user)
+
+        return False
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -31,6 +55,7 @@ class IngredientInRecipeSerializer(serializers.ModelSerializer):
 
 class RecipeSerializer(serializers.ModelSerializer):
     tags = TagSerializer(many=True)
+    author = UserSerializer()
     ingredients = IngredientInRecipeSerializer(
         source='ingredientinrecipe_set', many=True
     )
