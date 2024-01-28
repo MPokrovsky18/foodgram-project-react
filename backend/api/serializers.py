f5315c19fdfcb532f569b6e7691c618a055c38f2
Maxim Pokrovsky <maxim.pokrovsky1807@gmail.com>
2024-01-28 19:55:51 +0300

    Custom creating and updating of recipe.

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,7 @@
 import base64
 
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from django.core.files.base import ContentFile
 from rest_framework import serializers
 
@@ -46,6 +47,12 @@ class TagSerializer(serializers.ModelSerializer):
         model = Tag
         fields = ('id', 'name', 'color', 'slug')
 
+    def to_internal_value(self, data):
+        if not isinstance(data, int):
+            raise serializers.ValidationError("Некорректные данные для тега.")
+
+        return data
+
 
 class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
@@ -66,10 +73,31 @@ class IngredientInRecipeSerializer(serializers.ModelSerializer):
         model = IngredientInRecipe
         fields = ('id', 'name', 'measurement_unit', 'amount')
 
+    def to_internal_value(self, data):
+        print(data)
+        if (
+            not isinstance(data, dict)
+            or 'id' not in data
+            or 'amount' not in data
+            or not isinstance(data['id'], int)
+            or (isinstance(data['amount'], str)
+                and not data['amount'].isdigit())
+            or (not isinstance(data['amount'], str)
+                and not isinstance(data['amount'], int))
+        ):
+            raise serializers.ValidationError(
+                "Некорректные данные для ингредиента."
+            )
+
+        if isinstance(data['amount'], str):
+            data['amount'] = int(data['amount'])
+
+        return data
+
 
 class RecipeSerializer(serializers.ModelSerializer):
     tags = TagSerializer(many=True)
-    author = UserSerializer()
+    author = UserSerializer(read_only=True)
     ingredients = IngredientInRecipeSerializer(
         source='ingredientinrecipe_set', many=True
     )
@@ -107,3 +135,62 @@ class RecipeSerializer(serializers.ModelSerializer):
             return recipe.is_added_to_shopping_list_by_user(current_user)
 
         return False
+
+    def create(self, validated_data):
+        if (
+            'ingredients' not in self.initial_data
+            or 'tags' not in self.initial_data
+        ):
+            raise ValidationError(
+                'Поля "ingredients" и "tags" являются обязательными.'
+            )
+
+        ingredients = validated_data.pop('ingredientinrecipe_set')
+        tags = validated_data.pop('tags')
+
+        recipe = Recipe.objects.create(**validated_data)
+
+        for data_ingredient in ingredients:
+            IngredientInRecipe.objects.create(
+                recipe=recipe,
+                ingredient=Ingredient.objects.get(id=data_ingredient['id']),
+                amount=data_ingredient['amount']
+            )
+
+        for tag_id in tags:
+            recipe.tags.add(Tag.objects.get(id=tag_id))
+
+        return recipe
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.text = validated_data.get('text', instance.text)
+        instance.cooking_time = validated_data.get(
+            'cooking_time', instance.cooking_time
+        )
+        instance.image = validated_data.get('image', instance.image)
+
+        ingredients = validated_data.get('ingredientinrecipe_set')
+        tags = validated_data.get('tags')
+
+        if ingredients:
+            instance.ingredientinrecipe_set.all().delete()
+
+            for data_ingredient in ingredients:
+                IngredientInRecipe.objects.create(
+                    recipe=instance,
+                    ingredient=Ingredient.objects.get(
+                        id=data_ingredient['id']
+                    ),
+                    amount=data_ingredient['amount']
+                )
+
+        if tags:
+            instance.tags.clear()
+
+            for tag_id in tags:
+                instance.tags.add(Tag.objects.get(id=tag_id))
+
+        instance.save()
+
+        return instance
