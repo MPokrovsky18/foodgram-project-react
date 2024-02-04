504480358f30164f066a8a75125192dc2dd46ba5
Maxim Pokrovsky <maxim.pokrovsky1807@gmail.com>
2024-02-04 12:28:35 +0300

    Fix users app.

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,28 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group
+
 from users.models import FoodgramUser
 
 admin.site.unregister(Group)
 
-UserAdmin.fieldsets += (
-    ('Списки рецептов', {'fields': ('favorite_recipes', 'shopping_list')}),
-    ('Подписки', {'fields': ('subscriptions',)}),
-)
-
 
 @admin.register(FoodgramUser)
 class FoodgramUserAdmin(UserAdmin):
-    """
-    Admin interface customization for FoodgramUser model.
-
-    List display includes:
-        email, username, first name, last name, is_staff, and is_active.
-
-    Fieldsets include the default UserAdmin fieldsets
-    and additional fields for favorite_recipes and shopping_list.
-    """
-
     list_display = (
         'email',
         'username',
